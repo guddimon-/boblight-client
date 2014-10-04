c78906d8b4bd65904f748c8463fa5241d86dbfd5
guddimon- <github.guddimon-@gutmann-web.net>
2014-10-05 00:09:11 +0200

    created a thread for blinking of the power button added sig handler for a clean exit

--- client.py
+++ client.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+import sys
+import signal
 import time
 import lirc
 import threading
@@ -8,10 +10,152 @@ import ConfigParser
 
 import boblib
 
-class LircThread(threading.Thread):
+
+# initializie our global attributes
+led_blink_loop      = None
+led_blink           = None
+lirc_event_loop     = None
+switch_led_stripe   = None
+switch_power        = None
+bob                 = None
+relais_led_stripe   = None
+
+
+def __main__():
+    global led_blink_loop
+    global led_blink
+    global lirc_event_loop
+    global switch_led_stripe
+    global switch_power
+    global bob
+    global relais_led_stripe
+
+    # read configuration file
+    config = ConfigParser.ConfigParser()
+    config.read('boblight.cfg')
+    
+    switch_led_stripe   = config.getint('switch', 'switch_led_stripe')
+    switch_led          = config.getint('switch', 'switch_led')
+    relais_led_stripe   = config.getint('switch', 'relais_led_stripe')
+    switch_power        = config.getint('switch', 'switch_power')
+    
+    bob_host    = config.get('boblight', 'host')
+    bob_port    = config.getint('boblight', 'port')
+    
+    # initialize configured GPIO
+    GPIO.setmode(GPIO.BOARD)
+    
+    GPIO.setup(switch_led_stripe, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+    GPIO.add_event_detect(switch_led_stripe, GPIO.RISING, callback=switch_led_pushed, bouncetime=500)
+    
+    GPIO.setup(switch_led, GPIO.OUT)
+    
+    GPIO.setup(relais_led_stripe, GPIO.OUT, initial=0)
+    
+    GPIO.setup(switch_power, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+    GPIO.add_event_detect(switch_power, GPIO.RISING, callback=switch_power_pushed, bouncetime=500)
+    
+    # start blinking 
+    led_blink = GPIO.PWM(switch_led, 100)
+    led_blink.start(0)
+    
+    led_blink_loop = BlinkThread()
+    led_blink_loop.start()
+    
+    # start thread for IR events
+    lirc_event_loop = LircThread("boblight")
+    lirc_event_loop.start()
+    
+    # initialize boblight
+    bob = boblib.Boblight(bob_host, bob_port)
+    bob_client_init()
+    
+    for sig in [signal.SIGTERM, signal.SIGINT, signal.SIGHUP, signal.SIGQUIT]:
+        signal.signal(sig, handler)
+    
+    try:
+        while 1:
+            time.sleep(1)
+    
+    except:
+        pass
+    
+    
+
+
+def handler(signum = None, frame = None):
+    global led_blink_loop
+    global led_blink
+    global lirc_event_loop
+    global switch_led_stripe
+    global switch_power
+    global bob
+
+    print "Exit in progress..."
+
+    # cleanup everything we used
+    bob.disconnect()
+    
+    led_blink_loop.stop()
+    led_blink.stop()
+    
+    lirc_event_loop.stop()
+    
+    GPIO.remove_event_detect(switch_led_stripe)
+    GPIO.remove_event_detect(switch_power)
+    GPIO.cleanup()
+
+    print "Exit successful."
+    
+    sys.exit()
+
+class BlinkThread(threading.Thread):
+    global led_blink
+    global relais_led_stripe
+    
+    
     def run(self):
         self.run = True
-        self.sockid = lirc.init("boblight", blocking=False, verbose=True)
+        
+        while self.run:
+            if not GPIO.input(relais_led_stripe):
+                led_blink.ChangeFrequency(200)
+                for dc in range (90, 9, -5):
+                    led_blink.ChangeDutyCycle(dc)
+                    time.sleep(0.3)
+                time.sleep(5)
+                led_blink.ChangeFrequency(200)
+                for dc in range (10, 91, 5):
+                    led_blink.ChangeDutyCycle(dc)
+                    time.sleep(0.1)
+                time.sleep(1)
+            else:
+                led_blink.ChangeDutyCycle(10)
+                time.sleep(10)
+                
+        print "BlinkThread stopped."
+
+    def stop(self):
+        print "Stopping BlinkThread..."
+        self.run = False
+
+
+class LircThread(threading.Thread):
+    global led_blink_loop
+    global led_blink
+    global lirc_event_loop
+    global switch_led_stripe
+    global switch_power
+    global bob
+
+
+    def __init__(self, name_ir):
+        super(LircThread, self).__init__()
+        self.sockid = lirc.init(name_ir, blocking=False, verbose=True)
+
+
+    def run(self):
+        self.run = True
 
         while self.run:
             event = lirc.nextcode()
@@ -135,8 +279,11 @@ class LircThread(threading.Thread):
                         print e
 
             time.sleep(0.1)
+            
+        print "LircThread stopped."
 
     def stop(self):
+        print "Stopping LircThread..."
         self.run = False
 
 
@@ -149,6 +296,7 @@ def switch_power_pushed(switch_power=None):
 
 def bob_client_init():
     global bob
+    
     bob.setPriority(255)
     bob.setInterpolation(0)
     bob.setSpeed(100)
@@ -157,69 +305,4 @@ def bob_client_init():
     bob.setSpeed(0)
 
 
-# read configuration file
-config = ConfigParser.ConfigParser()
-config.read('boblight.cfg')
-
-switch_led_stripe   = config.getint('switch', 'switch_led_stripe')
-switch_led          = config.getint('switch', 'switch_led')
-relais_led_stripe   = config.getint('switch', 'relais_led_stripe')
-switch_power        = config.getint('switch', 'switch_power')
-
-bob_host    = config.get('boblight', 'host')
-bob_port    = config.getint('boblight', 'port')
-
-# initialize configured GPIO
-GPIO.setmode(GPIO.BOARD)
-
-GPIO.setup(switch_led_stripe, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.add_event_detect(switch_led_stripe, GPIO.RISING, callback=switch_led_pushed, bouncetime=500)
-
-GPIO.setup(switch_led, GPIO.OUT)
-
-GPIO.setup(relais_led_stripe, GPIO.OUT, initial=0)
-
-GPIO.setup(switch_power, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.add_event_detect(switch_power, GPIO.RISING, callback=switch_power_pushed, bouncetime=500)
-
-# start blinking 
-led_blink = GPIO.PWM(switch_led, 100)
-led_blink.start(0)
-
-# start thread for IR events
-lirc_event_loop = LircThread()
-lirc_event_loop.start()
-
-# initialize boblight
-bob = boblib.Boblight(bob_host, bob_port)
-bob_client_init()
-
-try:
-    while 1:
-        if not GPIO.input(relais_led_stripe):
-            led_blink.ChangeFrequency(200)
-            for dc in range (90, 9, -5):
-                led_blink.ChangeDutyCycle(dc)
-                time.sleep(0.3)
-            time.sleep(5)
-            led_blink.ChangeFrequency(200)
-            for dc in range (10, 91, 5):
-                led_blink.ChangeDutyCycle(dc)
-                time.sleep(0.1)
-            time.sleep(1)
-        else:
-            led_blink.ChangeDutyCycle(10)
-            time.sleep(10)
-
-except:
-    pass
-
-
-# cleanup everything we used
-bob.disconnect()
-
-lirc_event_loop.stop()
-
-GPIO.remove_event_detect(switch_led_stripe)
-GPIO.remove_event_detect(switch_power)
-GPIO.cleanup()
+__main__()
